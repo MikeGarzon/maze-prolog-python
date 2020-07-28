ce171efbde72185067475bcc068c21a8b6c8fc5e
Mike <maicolgarzon3@gmail.com>
2020-07-28 12:36:26 -0500

    Final

--- laberinto.py
+++ laberinto.py
@@ -1,87 +1,83 @@
 from turtle import TurtleScreen, RawTurtle, TK
 from time import sleep
- 
+
 class Ventana():
-    
- 
+
+
     def __init__(self, titulo, alto, ancho):
-        
+
         assert isinstance(titulo, str)
         assert isinstance(alto, int) and alto > 0
         assert isinstance(ancho, int) and ancho > 0
- 
-        ## Crea la ventana y un canvas para dibujar
+
         self.root = TK.Tk()
         self.root.title(titulo)
         self.canvas = TK.Canvas(self.root, width=ancho, height=alto)
         self.canvas.pack()
-         
-        ## Crea un TurtleScreen y la tortuga para dibujar
+
         self.fondo_ventana = TurtleScreen(self.canvas)
         self.fondo_ventana.setworldcoordinates(0, alto, ancho, 0)
- 
-        ## Establece el color de fondo
-        self.canvas["bg"] = "blue"
+
+        self.canvas["bg"] = "gold"
         self.canvas.pack()
- 
-        ## Crea una tortuga para dibujar
+
         self.pencil = RawTurtle(self.fondo_ventana)
         self.pencil.pencolor("white")
 
-class Laberinto():    
-    
+class Laberinto():
+
     Xdis = 10
     Ydis = 10
 
     Alto = 25
     Ancho= 25
- 
+
     @staticmethod
-    def deme_posicion(i, j):        
+    def deme_posicion(i, j):
         x = Laberinto.Xdis + j * (Laberinto.Ancho + 1)
         y = Laberinto.Ydis + i * (Laberinto.Alto  + 1)
         return (x, y)
- 
-             
+
+
     def __init__(self, area_dibujo, laberinto):
-                
+
         lista = laberinto.split()
         lista = [ x[:-1] if x[-1] == "\n" else x for x in lista]
-        lista = [[int(ch) for ch in x] for x in lista] 
- 
+        lista = [[int(ch) for ch in x] for x in lista]
+
         self.laberinto = lista
         self.lienzo = area_dibujo
 
         self.dibuja_laberinto()
- 
+
     def dibuja_laberinto(self):
-        
+
         self.lienzo.fondo_ventana.tracer(False)
         self.lienzo.pencil.pencolor("white")
- 
+
         for i in range(len(self.laberinto)):
- 
+
             for j in range(len(self.laberinto[i])):
- 
+
                 if self.laberinto[i][j] == 1:
                     self.casilla("black", i, j)
                 elif self.laberinto[i][j] == 3:
                     self.casilla("red", i, j)
                 elif self.laberinto[i][j] == 0:
                     self.casilla("white", i, j)
- 
+
         self.lienzo.fondo_ventana.tracer(True)
- 
+
     def casilla(self, color, i, j):
-        
-        x, y = Laberinto.deme_posicion(i, j) 
-        
+
+        x, y = Laberinto.deme_posicion(i, j)
+
         self.lienzo.pencil.fillcolor(color)
         self.lienzo.pencil.pu()
         self.lienzo.pencil.setpos(x, y)
         self.lienzo.pencil.seth(0)
         self.lienzo.pencil.pd()
-        self.lienzo.pencil.begin_fill() 
+        self.lienzo.pencil.begin_fill()
 
         for i in range(2):
             self.lienzo.pencil.fd(Laberinto.Ancho+1)
@@ -90,72 +86,20 @@ class Laberinto():
             self.lienzo.pencil.left(90)
 
         self.lienzo.pencil.end_fill()
- 
-    def recorrido(self, i, j):       
- 
-        if self.laberinto[i][j] == 3:
-            return [(i, j)]
- 
-        if self.laberinto[i][j] == 1:
-            return []
- 
-        self.laberinto[i][j] = -1
- 
-        sleep(0.10)
-        self.lienzo.fondo_ventana.tracer(False)
-        self.casilla("green", i, j)
-        self.lienzo.fondo_ventana.tracer(True)
- 
-        if i > 0 and self.laberinto[i - 1][j] in [0, 3]:     # Norte
-            camino = self.recorrido(i - 1, j)
-            if camino: return [(i, j)] + camino
- 
-        if j < len(self.laberinto[i]) - 1 and \
-           self.laberinto[i][j + 1] in [0, 3]:               # Este
-            camino = self.recorrido(i, j + 1)
-            if camino: return [(i, j)] + camino
- 
-        if i < len(self.laberinto) - 1 and \
-           self.laberinto[i + 1][j] in [0, 3]:               # Sur
-            camino = self.recorrido(i + 1, j)
-            if camino: return [(i, j)] + camino
- 
-        if j > 0 and self.laberinto[i][j - 1] in [0, 3]:     # Oeste
-            camino = self.recorrido(i, j - 1) 
-            if camino: return [(i, j)] + camino
- 
-        sleep(0.10)
-        self.lienzo.fondo_ventana.tracer(False)
-        self.casilla("white", i, j)
-        self.lienzo.fondo_ventana.tracer(True)
- 
-        return []
- 
-    def reset(self):
- 
-        for i in range(len(self.laberinto)):
-            for j in range(len(self.laberinto[i])):
-                if self.laberinto[i][j] == -1:
-                    self.laberinto[i][j] = 0
 
-stringLab = "10111111111111\n" + \
-            "10000000010003\n" + \
-            "10111111010111\n" + \
-            "10100001010001\n" + \
-            "10111101111001\n" + \
-            "10000001010001\n" + \
-            "10111111010111\n" + \
-            "10000000010001\n" + \
-            "11110111111101\n" + \
-            "10000000000001\n" + \
-            "11111111111101\n" + \
-            "10000000000001\n" + \
-            "11111111111111" 
- 
-def principal():
-    ll = Laberinto(Ventana("Laberinto", 380, 380), stringLab)
-    ll.recorrido(0,1) ##Inicio
-    ll.reset()
- 
-if __name__ == "__main__":
-    principal()
+    def dibujar(self, sol):
+
+
+        sleep(1)
+        self.lienzo.fondo_ventana.tracer(False)
+        for i in range(len(sol)):
+            x = int(sol[i][0])
+            y = int(sol[i][1])
+            sleep(0.05)
+            self.casilla("darkgreen", x,y)
+            self.lienzo.fondo_ventana.tracer(True)
+        sleep(3)
+
+def principal(dibujo, sol):
+    ll = Laberinto(Ventana("Laberinto", 255, 235), dibujo)
+    ll.dibujar(sol)
--- maze.py
+++ maze.py
@@ -1,4 +1,5 @@
 from pyswip import Prolog
+from laberinto import principal
 
 def leerArchivo(archivo):
 	c = [line.splitlines() for line in (open(archivo, "r"))]
@@ -36,6 +37,19 @@ def ass(camino):
   for x in range(0 , len(camino) - 1):
     p.assertz("conecta("+camino[x][0]+","+camino[x+1][0]+")")
 
+def solu(camino):
+  s = []
+  aux =[]
+
+  for x in range(0 , len(camino)):
+    for j in range(0 , len(camino[x]) ):
+      if (camino[x][j] == 'o' or camino[x][j] == 'f'):
+
+        aux = [str(x),str(j)]
+        s.append(aux)
+
+  return s
+
 def solucion():
 	solucion = []
 	for r in p.query("camino([i],Sol)"):
@@ -46,20 +60,40 @@ def solucion():
 	solucion[0] = 'f'
 	return solucion
 
-#----------------------------------------------------------------
+#-------------------------"MAIN"----------------------------------
 
 p = Prolog()
 p.consult('laberinto.pl')
 
 maze = leerArchivo("maze.txt")  #CON ESTE CREE EL MAPA
 
+#----------------------------------------------------------------
+
+dibujo = ''                     #PARA DIBUJAR EL MAPA
+
+for i in range(len(maze)):
+    for j in range(len(maze[i])):
+        if (maze[i][j] == '|' or maze[i][j] ==  '--') : dibujo += '1'
+        elif maze[i][j] == 'f': dibujo+= '3'
+        else : dibujo += '0'
+    dibujo += '\n'
+
+#----------------------------------------------------------------
+
 camino = []
-encontrar(inicio(maze,0),maze)
+encontrar(inicio(maze,0),maze)    #CONECTA LOS CAMINOS
+
+#----------------------------------------------------------------
 
 while (['0'] in camino): camino.remove(['0']) #AQUI QUITO EL 0 DE LAS HORIZONTALES
 
-ass(list(camino))
+ass(list(camino))               #ASSERTZ's
 
-sol = solucion()[::-1]   #CON ESTE MUESTRA LA SOLUCION A TRAVES DEL MAPA
+sol = solucion()[::-1]   #Guardo lo que manda prolog
 
-print (sol)
+print(sol)
+
+#----------------------------------------------------------------
+
+principal(dibujo,solu(list(maze)))  #Dibuja el camino
+
